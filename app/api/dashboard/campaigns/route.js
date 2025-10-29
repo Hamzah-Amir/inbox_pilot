efad966230ae05ddd64deb818f5f7583bded1dff
Hamza <hamzaamir7651@gmail.com>
2025-10-28 22:20:24 -0700

    fixed issues in campaign fetching api route

--- app/api/dashboard/campaigns/route.js
+++ app/api/dashboard/campaigns/route.js
@@ -1,21 +1,23 @@
 import { getCampaign } from "@/actions/useractions";
 import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-    const session = await getServerSession()
+    const session = await getServerSession(authOptions)
+    const campaigns = await getCampaign(session.user.id);
 
-    if (!session) {
+    if (!session || !session.user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const campaigns = await getCampaign("cmh94mtka0000tze8g4a9rz99")
 
     return NextResponse.json(campaigns.map(c => {
         const replies = c.emails.reduce((acc, e) => acc + e.replies.length, 0);
         const emailSent = c.emailsSent;
 
         return {
+            id:c.id,
             title: c.title,
             status: c.status,
             emailSent: emailSent,
