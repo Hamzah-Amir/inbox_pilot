b1628b4b5996b21ae0cbdbb24026ece7c39426ec
Hamza <hamzaamir7651@gmail.com>
2025-10-29 05:39:33 -0700

    added a default tone

--- app/dashboard/generate/page.js
+++ app/dashboard/generate/page.js
@@ -32,7 +32,20 @@ const EmailGenerationPage = () => {
     }, [session])
 
     const onSubmit = async (data) => {
-        console.log(data)
+        const res = await fetch("/api/personalize", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                campaignId: data.campaignId,
+                recipentEmail: data.recipentEmail,
+                companyName: data.companyName,
+                recipentName: data.recipentName,
+                tone: data.tone,
+                productDescription: data.productDescription
+            })
+        })
     }
 
 
@@ -44,14 +57,14 @@ const EmailGenerationPage = () => {
                     <h1 className='text-4xl'>Generate AI Emails</h1>
                     <p className='text-gray-300 mt-2'>Create personalized emails for your selected campaign.</p>
                     <form onSubmit={handleSubmit(onSubmit)}
-                        className='my-6 w-[60vw] h-[60vh] mx-[12vw] border flex flex-col  border-[#030b1b] size-full p-6 rounded-[14px] shadow-[#030b1b] shadow-2xl bg-[#0B1624] '>
+                        className='my-6 w-[60vw] min-h-[90vh] mx-[12vw] border flex flex-col  border-[#030b1b] size-full p-6 rounded-[14px] shadow-[#030b1b] shadow-2xl bg-[#0B1624] '>
                         <h3 className='text-lg'>Email Configuration</h3>
                         <p className='text-sm text-gray-400 mb-8'>Set your preferences and let AI craft the perfect email.</p>
 
-                        <label htmlFor='campaign' className='block mb-2'>Select Campaign</label>
+                        <label htmlFor='campaignId' className='block mb-2'>Select Campaign</label>
                         <select className='border w-full mb-6 rounded-lg p-2 h-11 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none'
-                            id='status'
-                            {...register("campaigns", { required: true })}
+                            id='campaignId'
+                            {...register("campaignId", { required: true })}
                         >
                             {campaigns && campaigns.map(c => (
                                 <option key={c.id} value={c.id}>{c.title} ({c.goal})</option>
@@ -66,11 +79,27 @@ const EmailGenerationPage = () => {
                         />
                         {errors.recipentEmail && <p className="text-sm text-red-500 mt-1">Recipent Email is required!</p>}
 
-                        <label htmlFor='tone' className='block mb-2'>Select Campaign</label>
-                        <select className='border w-full rounded-lg p-2 h-11 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none'
+                        <label htmlFor='companyName' className='block mb-2'>Enter Campany Name <span className='text-red-500'>*</span></label>
+                        <input className='border w-full mb-6 rounded-lg p-2 h-11 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none'
+                            placeholder='Enter Campany Name'
+                            id='companyName'
+                            {...register("companyName", { required: true })}
+                        />
+
+                        <label htmlFor='recipentName' className='block mb-2'>Enter Recipent Name <span className='text-red-500'>*</span></label>
+                        <input className='border w-full mb-6 rounded-lg p-2 h-11 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none'
+                            placeholder='Enter Recipent Name'
+                            id='recipentName'
+                            {...register("recipentName", { required: false })}
+                        />
+
+
+                        <label htmlFor='tone' className='block mb-2'>Select Tone <span className='text-red-500'>*</span></label>
+                        <select className='border w-full mb-6 rounded-lg p-2 h-11 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none'
                             id='tone'
                             {...register("tone", { required: true })}
                         >
+                            <option value="Persuasive & Witty">Inbox Pilot's default</option>
                             <option value="Professional">Professional</option>
                             <option value="Persuasive">Persuasive</option>
                             <option value="Friendly">Friendly</option>
@@ -78,6 +107,19 @@ const EmailGenerationPage = () => {
                             <option value="Witty">Witty</option>
                             <option value="Data-Driven">Data-Driven</option>
                         </select>
+
+                        <label htmlFor='productDescription' className='block mb-2'>
+                            Enter Product Description <p className="text-gray-400 text-sm mb-2">
+                                (Optional, but helps generate more personalized emails)
+                            </p>
+                        </label>
+
+                        <textarea
+                            className='border w-full mb-6 rounded-lg p-2 h-28 placeholder:text-[#8b99ad] focus:border-[#22D3EE] outline-none resize-none'
+                            placeholder='Enter Product Description'
+                            id='productDescription'
+                            {...register("productDescription", { required: false })}
+                        />
 
                         <button className=" border mt-6 border-cyan-600 cursor-pointer rounded-lg p-2 px-6 font-bold bg-cyan-500"
                             type='submit'
