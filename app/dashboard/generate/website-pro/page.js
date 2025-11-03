0df260d1d53c78ec30dcf9c1a955dcacc1cc6e5a
Hamza <hamzaamir7651@gmail.com>
2025-11-03 23:11:48 +0500

    scrape website function

--- app/dashboard/generate/website-pro/page.js
+++ app/dashboard/generate/website-pro/page.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import * as cheerio from 'cheerio'
+import axios from 'axios'
 
 const WebsiteProPersonalization = () => {
+    
+    const scrapeWebsite = async (domain) =>{
+        try {
+            const url = domain.startsWith("http")?domain : `https://${domain}`
+            const { data } = await  
+        } catch (error) {
+            return null
+        }
+    }
+
   return (
     <>
     
