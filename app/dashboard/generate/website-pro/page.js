a2debda22f56728d7e81cf21873ca590ee4dc310
Hamza <hamzaamir7651@gmail.com>
2025-11-03 23:51:07 +0500

    made a basic logic for website scrapping

--- app/dashboard/generate/website-pro/page.js
+++ app/dashboard/generate/website-pro/page.js
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import * as cheerio from 'cheerio'
 import axios from 'axios'
@@ -7,7 +8,14 @@ const WebsiteProPersonalization = () => {
     const scrapeWebsite = async (domain) =>{
         try {
             const url = domain.startsWith("http")?domain : `https://${domain}`
-            const { data } = await  
+            const { data } = await axios.get(url, { timeout: 10000 });
+            const $ = cheerio.load(data)
+            console.log($)
+            const headline = $("h1").first().text().trim()
+            const subHeadline = $("h2").first().text().trim()
+            const pageText = $("body").text().replace(/\s+/g, " ").trim()
+            console.log("Heading: ", headline)
+            return {headline, subHeadline, pageText}
         } catch (error) {
             return null
         }
@@ -15,7 +23,9 @@ const WebsiteProPersonalization = () => {
 
   return (
     <>
-    
+    <button onClick={() => scrapeWebsite("http://shop-scribe-xi.vercel.app")}>
+    Web Scrapping
+    </button>
     </>
   )
 }
