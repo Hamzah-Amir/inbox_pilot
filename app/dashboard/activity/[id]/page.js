52f43f443bd28a103811b17470868abeefcb8756
Hamza <hamzaamir7651@gmail.com>
2025-11-07 10:29:12 +0500

    configure id page for both type of outputs

--- app/dashboard/activity/[id]/page.js
+++ app/dashboard/activity/[id]/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fetchEmailById } from '@/actions/useractions'
+import { fetchEmailQuality, fetchEmailById } from '@/actions/useractions'
 import Sidebar from '@/components/Sidebar'
 
 const ActivityEmailPage = async ({ params }) => {
@@ -7,6 +7,24 @@ const ActivityEmailPage = async ({ params }) => {
     const emailId = await params.id
 
     const email = await fetchEmailById(emailId)
+    const emailQuality = await fetchEmailQuality()
+
+    let subject;
+    let intro;
+    let body;
+    let closing;
+
+    const isTemplate = email.emailType === "TEMPLATE"
+    if (isTemplate) {
+        const parsed = JSON.parse(email.output)
+        subject = parsed.subject
+        intro = parsed.intro
+        body = parsed.body
+        closing = parsed.closing
+    } else {
+        body = email.output || ""
+        console.log("Body web", body)
+    }
 
     if (!email) {
         return (
@@ -25,10 +43,11 @@ const ActivityEmailPage = async ({ params }) => {
     const campaign = emailWithCampaign.campaign
 
 
-    const content = JSON.parse(email.output)
+    const content = email.output
+    console.log("Content", content)
     return (
         <>
-            <main className='mx-[17.5vw] min-h-screen'>
+            <main className='mx-[17vw] min-h-screen'>
                 <Sidebar />
                 <section className='min-w-[45vw] min-h-[70vh] mx-[15vw] my-[10vh] rounded-2xl border bg-[#101c33] border-gray-800'>
                     <div className='p-6'>
@@ -46,8 +65,10 @@ const ActivityEmailPage = async ({ params }) => {
                         </div>
                         <div className='border-b border-gray-600'></div>
                         <div className='flex items-center justify-between h-24'>
-                            <div>
-                                <p className='hover:underline hover:decoration-cyan-500 hover:decoration-2'>{content?.subject}</p>
+                            <div>{isTemplate && (
+                                <p className='hover:underline hover:decoration-cyan-500 hover:decoration-2'>{subject}</p>
+                            )
+                            }
                             </div>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
                                 <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
@@ -55,13 +76,47 @@ const ActivityEmailPage = async ({ params }) => {
                             </svg>
                         </div>
                         <div className='border-b border-gray-600'></div>
+                        {isTemplate}
                         <div className='mt-12'>
-                            <p>Hi {email.recipentName},</p>
-                            <p>{content.intro}</p>
-                            <p>{content.body}</p>
-                            <p>{content.closing}</p>
+                            {isTemplate && (
+                                <p>{intro}</p>
+                            )
+                            }
+                                <p>{body}</p>
+                            {isTemplate && (
+                                <p>{closing}</p>
+                            )}
                         </div>
                     </div>
+                </section>
+                <section className='mx-[15vw]'>
+                    <div className="mt-12 p-6 rounded-lg border border-gray-700 bg-[#0F1A2C]">
+                        <h4 className="text-lg font-semibold mb-3">Email Quality</h4>
+
+                        <div className="grid grid-cols-2 gap-4 text-sm text-gray-300">
+                            <div>
+                                <p className="font-medium text-white">Personalization Score</p>
+                                <p>{0}</p>
+                                {/* <p>{emailQuality.personalizationScore || '0'}</p> */}
+                            </div>
+                            <div>
+                                <p className="font-medium text-white">Website Context Score</p>
+                                {/* <p>{emailQuality.websiteContextScore || 0}</p> */}
+                                <p>{0}</p>
+                            </div>
+                            <div>
+                                <p className="font-medium text-white">Readability Grade (Flesch-Kincaid)</p>
+                                <p>{0}</p>
+                                {/* <p>{emailQuality.readabilityGrade.toFixed(2) || 0}</p> */}
+                            </div>
+                            <div>
+                                <p className="font-medium text-white">Tone Match</p>
+                                {/* <p>{emailQuality.toneMatched ? "Matched" : "Not Matched"}</p> */}
+                                <p>{"Not Matched"}</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </section>
             </main>
         </>
