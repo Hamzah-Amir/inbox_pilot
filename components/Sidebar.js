92ff9207ec7708b15d000fa08325fd46d80a713c
Hamza <hamzaamir7651@gmail.com>
2025-11-08 14:12:14 +0500

    added manage billing

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const Sidebar = () => {
+
   return (
     <aside id="default-sidebar" className="fixed border-t border-gray-700  top-[82px] left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
             <div className="h-full px-4 py-6 overflow-y-auto bg-[#071021] border-r border-gray-800 shadow-inner">
@@ -32,12 +33,12 @@ const Sidebar = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/settings" className="flex items-center p-3 text-gray-300 rounded-lg hover:bg-[#0f1724] hover:text-white group transition-colors">
+                  <button onClick={() => window.location.href = `https://app.lemonsqueezy.com/my/subscriptions/21512a`} className="flex items-center p-3 text-gray-300 rounded-lg hover:bg-[#0f1724] hover:text-white group transition-colors">
                     <svg className="shrink-0 w-5 h-5 text-gray-400 transition duration-150 group-hover:text-cyan-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                       <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                     </svg>
-                    <span className="flex-1 ms-3 whitespace-nowrap">Settings</span>
-                  </Link>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Manage Billing</span>
+                  </button>
                 </li>
              
               </ul>
