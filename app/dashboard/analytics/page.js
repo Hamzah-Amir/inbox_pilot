bf53b37573c3eacf53715843b63193ef97bddeee
Hamza <hamzaamir7651@gmail.com>
2025-11-08 14:11:55 +0500

    a spoiler for analytics page

---
+++ app/dashboard/analytics/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AnalyticsPage = () => {
+    return (
+        <>
+        <div className='min-h-screen flex justify-center items-center'>
+        <p className='text-4xl text-white'>
+            Coming Soon...
+        </p>
+        </div>
+        </>
+    )
+}
+
+export default AnalyticsPage
