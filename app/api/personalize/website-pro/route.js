91ec3389d62eba0c19f9a4a20caf7f8336cd47d2
Hamza <hamzaamir7651@gmail.com>
2025-11-09 13:37:01 +0500

    fixing servelss chroe error

--- app/api/personalize/website-pro/route.js
+++ app/api/personalize/website-pro/route.js
@@ -1,4 +1,4 @@
-export const runtime = "nodejs"
+export const runtime = "nodejs";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
