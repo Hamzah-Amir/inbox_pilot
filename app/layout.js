da2f42cda151749433c1b03984d801f2bce11d9e
Hamza <hamzaamir7651@gmail.com>
2025-11-09 09:27:48 +0500

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
