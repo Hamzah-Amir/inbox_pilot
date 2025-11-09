75518ca1abab1df7f12866ec6d8774db5d3aa493
Hamza <hamzaamir7651@gmail.com>
2025-11-09 14:47:29 +0500

    fixed some errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+//   serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
